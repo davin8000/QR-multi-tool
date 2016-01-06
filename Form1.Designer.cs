a77681f7dfe43e9ebeb347a9dbfcab1968e5a338
davin8000 <cs.davin@gmail.com>
2016-01-06 22:08:51 +0800

    Main Code Added
    Added Main Project

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace QR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxECC = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textQRCode = new System.Windows.Forms.RichTextBox();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxECC
+            // 
+            this.comboBoxECC.FormattingEnabled = true;
+            this.comboBoxECC.Items.AddRange(new object[] {
+            "L",
+            "M",
+            "Q",
+            "H"});
+            this.comboBoxECC.Location = new System.Drawing.Point(98, 6);
+            this.comboBoxECC.Name = "comboBoxECC";
+            this.comboBoxECC.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxECC.TabIndex = 0;
+            this.comboBoxECC.SelectedIndexChanged += new System.EventHandler(this.comboBoxECC_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Error Correction";
+            // 
+            // textQRCode
+            // 
+            this.textQRCode.Location = new System.Drawing.Point(12, 33);
+            this.textQRCode.Name = "textQRCode";
+            this.textQRCode.Size = new System.Drawing.Size(288, 259);
+            this.textQRCode.TabIndex = 3;
+            this.textQRCode.Text = "";
+            this.textQRCode.TextChanged += new System.EventHandler(this.textQRCode_TextChanged);
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(225, 4);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(75, 23);
+            this.buttonGenerate.TabIndex = 4;
+            this.buttonGenerate.Text = "Save";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(307, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(288, 288);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 299);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "QR Size: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(304, 299);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "QR Size: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 319);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonGenerate);
+            this.Controls.Add(this.textQRCode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxECC);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "QR Saver";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxECC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox textQRCode;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QRCoder;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace QR
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetDC(IntPtr hwnd);
+
+        Bitmap img;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxECC.SelectedIndex = 0;
+            set_comboBox_toolTip();
+            generate_QR();
+        }
+
+        private void renderQRCode()
+        {
+            string level = comboBoxECC.SelectedItem.ToString();
+            QRCodeGenerator.ECCLevel eccLevel = (QRCodeGenerator.ECCLevel)(level == "L" ? 0 : level == "M" ? 1 : level == "Q" ? 2 : 3);
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(textQRCode.Text, eccLevel);
+            QRCode qrCode = new QRCode(qrCodeData);
+            img = qrCode.GetGraphic(20, Color.Black, Color.White);            
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            renderQRCode();
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "JPG | *.jpg";
+            if (dialog.ShowDialog() == DialogResult.OK)
+                img.Save(dialog.FileName, ImageFormat.Jpeg);
+        }
+        
+        private void set_comboBox_toolTip()
+        {
+            string level = comboBoxECC.SelectedItem.ToString();
+            ToolTip toolTip1 = new ToolTip();
+            toolTip1.AutoPopDelay = 5000;
+            toolTip1.InitialDelay = 1000;
+            toolTip1.ReshowDelay = 500;
+            toolTip1.ShowAlways = true;
+            toolTip1.SetToolTip(this.comboBoxECC, (level == "L" ? "Can receive up to 7% Damage" : level == "M" ? "Can receive up to 15% Damage" : level == "Q" ? "Can receive up to 25% Damage" : "Can receive up to 30% Damage"));
+        }
+
+        private void generate_QR()
+        {
+            Single xDpi, yDpi;
+            IntPtr dc = GetDC(IntPtr.Zero);
+            using (Graphics g = Graphics.FromHdc(dc))
+            {
+                xDpi = g.DpiX;
+                yDpi = g.DpiY;
+            }
+            renderQRCode();
+            label2.Text = "QR Size(PIXEL)(HxW): " + img.Size.Height + "x" + img.Size.Width;
+            label3.Text = "QR Size(INCH)(XxY): " + (img.Size.Width / xDpi) + "x" + (img.Size.Height / yDpi);
+            Bitmap pic = new Bitmap(img, pictureBox1.Size);
+            pictureBox1.BackgroundImage = pic;
+        }
+
+        private void comboBoxECC_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            set_comboBox_toolTip();
+            generate_QR();
+        }
+
+        private void textQRCode_TextChanged(object sender, EventArgs e)
+        {
+            generate_QR();
+        }        
+    }
+}
